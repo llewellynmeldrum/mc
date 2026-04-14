c55870e7448f1ada71c0d7a7ba77197743228f42
llewellynmeldrum <llewellyn.meldrum1@students.mq.edu.au>
2026-04-14 10:02:59 +1000

    Updated src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,7 @@ int main(int argc, char** argv) {
     } 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_RESIZABLE, false);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow* window = glfwCreateWindow(640, 480, "Window Title", NULL, NULL);
     if (!window) {
