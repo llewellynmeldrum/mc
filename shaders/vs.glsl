e1a114cae8f5818187f14d90255c9eddec1a50b7
llewellynmeldrum <llewellyn.meldrum1@students.mq.edu.au>
2026-04-17 20:01:06 +1000

    meant for last commit

--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -5,9 +5,23 @@ layout (location = 2) in vec3 vs_vertColor;
 
 out vec2 texCoord;
 
-uniform mat4 scale;
+uniform int triangle_idx;
+
+uniform mat4 scale0;
+uniform mat4 scale1;
 uniform mat4 rotation;
+uniform mat4 mouse_transform;
 void main(){
-    gl_Position = scale * rotation * vec4(pos.xyz, 1.0);
+    switch (triangle_idx){
+        case 0:
+            gl_Position = scale0 * vec4(pos.xyz, 1.0);
+        break;
+        case 1: 
+            gl_Position = scale1 * rotation * vec4(pos.xyz, 1.0);
+        break;
+        case 2: 
+            gl_Position = mouse_transform * vec4(pos.xyz, 1.0);
+        break;
+    }
     texCoord = vs_texCoord;
 }
